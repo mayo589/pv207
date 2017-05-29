bd1352cf0e4c6345864469429d99b6145ecab421
pv207 <>
2017-05-29 17:59:27 +0200

    Create new inactiveAccount data model {/DataModel/src/main/java/org/muni/fi/datamodel/InactiveAccount.java}

--- DataModel/src/main/java/org/muni/fi/datamodel/InactiveAccount.java
+++ DataModel/src/main/java/org/muni/fi/datamodel/InactiveAccount.java
@@ -5,28 +5,76 @@ package org.muni.fi.datamodel;
  */
 
 @javax.persistence.Entity
-public class InactiveAccount implements java.io.Serializable {
+public class InactiveAccount implements java.io.Serializable
+{
 
-    static final long serialVersionUID = 1L;
+   static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "INACTIVEACCOUNT_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(name = "INACTIVEACCOUNT_ID_GENERATOR", sequenceName = "INACTIVEACCOUNT_ID_SEQ")
-    private java.lang.Long id;
+   @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "INACTIVEACCOUNT_ID_GENERATOR")
+   @javax.persistence.Id
+   @javax.persistence.SequenceGenerator(name = "INACTIVEACCOUNT_ID_GENERATOR", sequenceName = "INACTIVEACCOUNT_ID_SEQ")
+   private java.lang.Long id;
 
-    public InactiveAccount() {
-    }
-    
-    public InactiveAccount(java.lang.Long id) {
-        this.id = id;
-    }
+   @org.kie.api.definition.type.Label(value = "firstName")
+   private java.lang.String firstName;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+   @org.kie.api.definition.type.Label(value = "lastName")
+   private java.lang.String lastName;
+
+   @org.kie.api.definition.type.Label(value = "email")
+   private java.lang.String email;
+
+   public InactiveAccount()
+   {
+   }
+
+   public java.lang.Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(java.lang.Long id)
+   {
+      this.id = id;
+   }
+
+   public java.lang.String getFirstName()
+   {
+      return this.firstName;
+   }
+
+   public void setFirstName(java.lang.String firstName)
+   {
+      this.firstName = firstName;
+   }
+
+   public java.lang.String getLastName()
+   {
+      return this.lastName;
+   }
+
+   public void setLastName(java.lang.String lastName)
+   {
+      this.lastName = lastName;
+   }
+
+   public java.lang.String getEmail()
+   {
+      return this.email;
+   }
+
+   public void setEmail(java.lang.String email)
+   {
+      this.email = email;
+   }
+
+   public InactiveAccount(java.lang.Long id, java.lang.String firstName,
+         java.lang.String lastName, java.lang.String email)
+   {
+      this.id = id;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+   }
 
 }
